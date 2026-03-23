8af7ab184b04c96aa9c3d32745e15296cf978fb3
Adam-Ghub <adam.sikora6002@gmail.com>
2026-03-23 19:16:31 +0100

    Google Analytics Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -119,6 +120,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+       <GoogleAnalytics gaId="G-LKEPBWPWTK"/>
     </html>
   );
 }
